8b2b8f920a4402135b607c58b9b3190a37c38bf5
Beatriz <1416962@senati.pe>
2025-03-31 10:45:21 -0500

    mi ultimo commit dx

--- src/crud/senati/CrudSenati.java
+++ src/crud/senati/CrudSenati.java
@@ -15,6 +15,8 @@ public class CrudSenati {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       Conexion cn=new Conexion ();
+       cn.conectar();
     }
     
 }
